16ec2db0d2ec508392514dd2c3c06eb2462414e0
hyunnn0105 <asha010519@gmail.com>
2022-12-16 01:03:30 +0900

    평점계산기

--- src/something/S_평점계산기.java
+++ src/something/S_평점계산기.java
@@ -1,12 +1,20 @@
 package something;
 
+import java.util.Scanner;
+
 public class S_평점계산기 {
     public static void main(String[] args) {
 
         // 심사위원 10명 -> 0 ~100 = 배열 A
         // 최저점수 제외, 최고점수 제외 ==> 평균
 
-        int[] A = {50,50,10,50,50,100,50,50,50,50};
+        // int[] A = {50,50,10,50,50,100,50,50,50,50};
+        int[] A = new int[10];
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i < A.length; i++) {
+            A[i] = sc.nextInt();
+        }
 
         // 대소 비교
         int max = 0;
